f7bf562f0e13d40c82c50e9926ab4fa7bbe7cf26
abhi7cr <abhi7cronaldo@gmail.com>
2018-04-17 15:32:01 -0500

    second question, basic styling

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 onCapitalQChange = () => {
   const answer = document.getElementById('capitalQ').value
   if (answer && answer.toLowerCase() === "antananarivo") {
-    document.getElementById('answer').textContent = 'Correct Answer!'
-    document.getElementById('answer').style.color = 'green'
+    document.getElementById('answer1').textContent = 'Correct Answer!'
+    document.getElementById('answer1').style.color = 'green'
   } else {
-    document.getElementById('answer').textContent = 'Oops, Incorrect Answer, Try again!'
-    document.getElementById('answer').style.color = 'red'
+    document.getElementById('answer1').textContent = 'Oops, Incorrect Answer, Try again!'
+    document.getElementById('answer1').style.color = 'red'
   }
 }
+
+onSecondHighestMountainChange = () => {
+    const answer = document.getElementById('secondHighestQ').value
+    if (answer && answer.toLowerCase() === "k2" || answer.toLowerCase() === "godwin austin") {
+      document.getElementById('answer2').textContent = 'Correct Answer!'
+      document.getElementById('answer2').style.color = 'green'
+    } else {
+      document.getElementById('answer2').textContent = 'Oops, Incorrect Answer, Try again!'
+      document.getElementById('answer2').style.color = 'red'
+    }
+  }
